e3b604a740ca8ffd593dc6c568bb24d0cef8a940
Matthew Bellario <mbellario@hawk.iit.edu>
2022-03-24 23:27:23 -0500

    add js and removed repeated info from shipping and billing

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
 'use strict'
+
+//Shipping elements
+var fullName = document.querySelector('#fullNamename');
+var address = document.querySelector('#address');
+var city = document.querySelector('#city');
+var state = document.querySelector('#state');
+var zip = document.querySelector('#zip');
+
+//Billing elements
+var cardholder = document.querySelector('#cardholder');
+var cardNumber = document.querySelector('#cardnum');
+var cardCCV = document.querySelector('#ccv');
+var cardExp = document.querySelector('#expiration');
+var cardExp = document.querySelector('#expiration');
+
+// .addeventlistener
